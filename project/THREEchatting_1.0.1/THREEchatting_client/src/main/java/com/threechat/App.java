6bb3c2b4172983325c262b6911f553fb32ff48bb
lixin03 <13149229812@163.com>
2018-11-06 15:51:24 +0800

    初始化

---
+++ project/THREEchatting_1.0.1/THREEchatting_client/src/main/java/com/threechat/App.java
@@ -0,0 +1,13 @@
+package com.threechat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
