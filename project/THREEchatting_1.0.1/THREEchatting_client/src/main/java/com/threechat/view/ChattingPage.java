4e3a79784d5e4f640bed414833092d8e776dd4d2
lixin03 <13149229812@163.com>
2018-11-06 23:44:40 +0800

    3

---
+++ project/THREEchatting_1.0.1/THREEchatting_client/src/main/java/com/threechat/view/ChattingPage.java
@@ -0,0 +1,17 @@
+package com.threechat.view;
+
+import javax.swing.JFrame;
+
+public class ChattingPage extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public ChattingPage() {
+		init();
+	}
+	private void init() {
+		
+	}
+}
---
+++ project/THREEchatting_1.0.1/THREEchatting_client/src/main/java/com/threechat/view/common/FontUtil.java
@@ -0,0 +1,10 @@
+package com.threechat.view.common;
+
+public class FontUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
